776a8d386ac32d037dbe9c4e2d24b2e681819156
AlexisBousquet <79169810+AlexisBousquet@users.noreply.github.com>
2021-03-02 23:13:49 +0100

    Delete PassengerDAO.java

--- PassengerDAO.java
+++
@@ -1,8 +0,0 @@
-
-public interface PassengerDAO {
-	/*renvoie le passager associé a cette ID*/
-	Passenger getPassenger(String id);
-	
-	/*Renvoie l'email associé a cet ID*/
-	String getEmail(String id);
-}
